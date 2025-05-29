69709bb388fa99b5c12bb611119210bdb3b8b17b
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-05-28 22:33:20 -0400

    variable and type - done

---
+++ variable-types/variable.py
@@ -0,0 +1,22 @@
+# integer
+myint = 7
+print(myint)
+# float
+myfloatNumber = 7.0
+print(myfloatNumber)
+#string
+stringTypeOne = 'typeOne'
+stringTypeTwo = "typeTwo"
+print(stringTypeOne)
+print(stringTypeTwo)
+# sum of numbers
+one = 1
+two = 2
+three = one + two
+print(three)
+#concatingating string
+hello = "hello"
+world = "world"
+print(hello + ' '+ world)
+#printing more than one variable in 
+print(one, two)
