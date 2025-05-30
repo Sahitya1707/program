e117c34f19309f26f2018a1bb10a6635234fd9e3
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-05-30 07:25:44 -0400

    operator done

---
+++ operators/script.py
@@ -0,0 +1,18 @@
+#we will learn about operator in this 
+number = 1 + 2 - 1 / 2 * 2
+print(number)
+
+# % modulo
+remainder = 11%2
+print(remainder)
+
+#using operator with string
+value1 = "value"
+value2 = "value2"
+print(value1 + " " + value2)
+
+# let's try operator with list
+even_number = [2,4,6,8]
+odd_number = [1,3,5,7]
+all_number = odd_number + even_number
+print(all_number)
