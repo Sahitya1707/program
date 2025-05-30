9ef9cfdc60cc151a1ba9c9b7f91da3bd4f761f36
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-05-30 09:13:20 -0400

    function made

---
+++ functions/script.py
@@ -0,0 +1,16 @@
+# functions in python are defined using the keyword def with functions name 
+def my_function():
+    print('Hello this my first function')
+
+#  invoking a function 
+my_function()
+
+# let's pass an argument inside the function
+def my_second_function(name, age):
+    print('My name is %s and age is %d' %(name, age))
+
+
+
+
+my_second_function("hari", 20)
+my_second_function('ram', 20)
